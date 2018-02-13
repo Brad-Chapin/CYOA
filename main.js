60a9b0a9d2eec607cfafd69cb1344138e51fca62
Brad Chapin <paledoor@yahoo.com>
2018-02-13 09:26:20 -0800

    building logic and display, first deployment

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  let charName = $("#cname");
+  $("#nbtn").on("click", function(){
+    event.preventDefault();
+    if (charName.val() == ""){
+      $("form").append("<p>You cannot enter the wilds without a name. Please tell us what to call you.</p>").append("<button id= 'ok'>Try again.</button");
+    } else {
+      $("#char-name").val() = charName.val();
+    }
+  });
 });
