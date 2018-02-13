3feff885657cb22827cefbb2a026927664693b34
Brad Chapin <paledoor@yahoo.com>
2018-02-13 10:35:25 -0800

    building up player input functionality

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 $( document ).ready(function() {
-  let charName = $("#cname");
+  showSub();
+  let charName = $("#cname"), sub = $("#subtitle"), charID = $("#idLine");
+
+  function showSub () {
+    setTimeout(function(){
+      sub.show();
+    }, 1500);
+  }
+
   $("#nbtn").on("click", function(){
     event.preventDefault();
     if (charName.val() == ""){
-      $("form").append("<p>You cannot enter the wilds without a name. Please tell us what to call you.</p>").append("<button id= 'ok'>Try again.</button");
+      $("#nmform").append("<p>You cannot enter the wilds without a name. Please tell us what to call you.</p>").append("<button id= 'ok'>Try again.</button");
     } else {
-      $("#char-name").val() = charName.val();
+      charID.append("<span>You are " + charName.val() +"</span>");
+      $("#nmform").addClass("hide");
+      $("#raceform").removeClass("hide");
     }
+  });
+
+  $("#raceform").on("click", function(){
+    event.preventDefault();
+    console.log(event.target.innerHTML);    charID.append("<span>, the " + event.target.innerHTML + ". " + "</span>")
+    $("#raceform").hide();
   });
 });
